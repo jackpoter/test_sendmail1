210de90426e2c5b2fe9e53cad30fd6544fbdc340
jack poter <onyourpv@gmail.com>
2014-01-22 12:43:02 +0800

    Update test_sendmail1.py

--- test_sendmail1.py
+++ test_sendmail1.py
@@ -1,27 +1,47 @@
-send_mail_host = 'smtp.gmail.com'
-send_mail_user = 'goodman'
-send_mail_user_name = u'goodman bbs'
-send_mail_pswd = '12345678'
-send_mail_postfix = 'gmail.com'
-get_mail_user = 'goodman'
-charset = 'utf-8'
+# -*- coding: utf-8 -*-
+import smtplib, email
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-get_mail_postfix = 'gmail.com'
-get_mail_host = 'smtp.gmail.com'
-def send(sub, content, reciver = get_mail_user + get_mail_postfix):
-send_mail_address = send_mail_user_name + '<' + send_mail_user + '@' + send_mail_postfix + '>'
-msg = email.mime.text.MIMEText(content,'html',charset)
-msg['Subject'] = email.Header.Header(sub,charset)
-msg['From'] = send_mail_address
-msg['to'] = to_adress = reciver
-try:
-#smtp = smtplib.SMTP(smtp_google, smtp_port)
-stp = smtplib.SMTP(send_mail_host,587)
-stp.connect(send_mail_host)
-stp.login(send_mail_user,send_mail_pswd)
-stp.sendmail(send_mail_address,to_adress,msg.as_string())
-stp.close()
-return True
-except Exception,e:
-print(e)
-return False
+def send(sub, smtp_debug, content, send_mail_user_name, send_mail_pswd, send_mail_postfix, get_mail_user, get_mail_postfix, charset):
+    send_mail_address = send_mail_user_name + '@' + send_mail_postfix
+    to_adress = get_mail_user + '@' + get_mail_postfix
+    #msg = email.mime.text.MIMEText(content,'html',charset)
+    msg = MIMEMultipart()
+    msg['Subject'] = email.Header.Header(sub,charset)
+    msg['From'] = send_mail_address
+    msg['To'] = to_adress
+    Contents = MIMEText(sub,'plain')      #
+    msg.attach(Contents)
+    try:
+        smtp = smtplib.SMTP(send_mail_host, 587)
+        if smtp_debug == 1:
+            smtp.set_debuglevel(1)
+        smtp.ehlo()  
+        smtp.starttls()  
+        smtp.ehlo()  
+        smtp.login(send_mail_user_name, send_mail_pswd)  
+        print 'login ok'
+        smtp.sendmail(send_mail_address, to_adress, msg.as_string())
+        print 'send ok'
+        return True
+    except Exception,e:
+        print 'sendmail Error:', e
+        return False
+
+if __name__ == "__main__":
+    
+    send_mail_host = 'smtp.gmail.com'
+    #send_mail_user = 'goodman'
+    #send_mail_user_name = u'goodman bbs'
+    send_mail_user_name = 'goodman_bbs'
+    send_mail_pswd = '123123123'
+    send_mail_postfix = 'gmail.com'
+    smtp_debug = 1
+    charset = 'utf-8'
+    sub = 'Hi there'
+
+    get_mail_user = 'goodman_save'
+    get_mail_postfix = 'gmail.com'
+    get_mail_host = 'pop.gmail.com'
+    send(sub, smtp_debug, content, send_mail_user_name, send_mail_pswd, send_mail_postfix, get_mail_user, get_mail_postfix, charset)
